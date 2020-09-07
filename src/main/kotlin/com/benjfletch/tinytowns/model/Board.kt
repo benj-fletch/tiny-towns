14684ff129ebb3c9cbc2e56a438fa350a3eea1d0
Ben F <ben.fletcher@dmwgroup.co.uk>
2020-09-07 14:20:45 +0100

    Adding ability to place GamePieces

--- src/main/kotlin/com/benjfletch/tinytowns/model/Board.kt
+++ src/main/kotlin/com/benjfletch/tinytowns/model/Board.kt
@@ -2,22 +2,34 @@ package com.benjfletch.tinytowns.model
 
 import com.benjfletch.tinytowns.BoardException
 
-/** Container for the status of a given Location on the [Board] */
-data class Content(val gamePiece: GamePiece?) {
-    companion object {
-        val NONE = Content(null)
-    }
-}
-
 /** Representation of the game board, with a configurable size */
 data class Board(private val size: Int = 4) {
-    val locations: Map<Pair<Int, Int>, Content>
+    val locations: MutableMap<Location, GamePiece?> = mutableMapOf()
 
     init {
         if(size < 1) throw BoardException("Board size $size is invalid. Must be > 1.")
-        locations = (1..size)
-                .flatMap { x -> (1..size).map { y -> x to y } }
-                .map { it to Content.NONE }
-                .toMap()
+        (0 until size)
+                .flatMap { x -> (0 until size).map { y -> Location(x, y) } }
+                .map { it to null }
+                .toMap(locations)
+    }
+
+    /**
+     * Place a [GamePiece] on to the board. This is only valid if:
+     *
+     * * [location] is a valid space on the board
+     * * [location] is not already occupied by a [GamePiece]
+     *
+     * in which case a [BoardException] is thrown.
+     */
+    fun place(location: Location, piece: GamePiece) {
+        if(!locations.containsKey(location)) {
+            throw BoardException("Cannot place ${piece.pieceName} at $location. Out of bounds.")
+        }
+        if(locations[location] != null) {
+            throw BoardException("Cannot place ${piece.pieceName} at $location. Place occupied.")
+        }
+
+        locations[location] = piece
     }
 }
