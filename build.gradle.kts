0304420619bf38806b7722325bac84157998c5f9
Ben F <ben.fletcher@dmwgroup.co.uk>
2020-09-07 13:13:05 +0100

    Add unit test GH Action

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,16 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    // Logging things
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    runtimeOnly("ch.qos.logback:logback-core:1.2.3")
+
+    // testing dependencies:
+    val jUnitVersion = "5.6.2"
+    testImplementation("org.junit.jupiter:junit-jupiter:${jUnitVersion}")
+    testImplementation("org.assertj:assertj-core:3.17.2")
 }
 
 repositories {
@@ -22,4 +32,8 @@ compileKotlin.kotlinOptions {
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
