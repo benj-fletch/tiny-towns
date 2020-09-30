6f41c64e90aff49b2fe232ea931597a5029af4f1
Ben F <ben.fletcher@dmwgroup.co.uk>
2020-09-30 17:14:28 +0100

    Adding basic scoring method

--- src/main/kotlin/com/benjfletch/tinytowns/model/buildings/Attractions.kt
+++ src/main/kotlin/com/benjfletch/tinytowns/model/buildings/Attractions.kt
@@ -1,5 +1,7 @@
 package com.benjfletch.tinytowns.model.buildings
 
+import com.benjfletch.tinytowns.model.GamePiece
+import com.benjfletch.tinytowns.model.Location
 import com.benjfletch.tinytowns.model.Resource.STONE
 import com.benjfletch.tinytowns.model.Resource.WOOD
 import com.benjfletch.tinytowns.model.Shape
@@ -10,6 +12,9 @@ object Fountain: Building {
     override val text = "2 (Point) if adjacent to a (Attraction)."
     override val shape = Shape(listOf(listOf(WOOD, STONE)))
     override val canBeBuiltAnywhere = false
+    override fun score(board: Map<Location, GamePiece>): Int {
+        return 0
+    }
 }
 
 object Millstone: Building {
@@ -18,6 +23,9 @@ object Millstone: Building {
     override val text = "2 (Point) if adjacent to a (FoodProducer) or (Shop)."
     override val shape = Shape(listOf(listOf(WOOD, STONE)))
     override val canBeBuiltAnywhere = false
+    override fun score(board: Map<Location, GamePiece>): Int {
+        return 0
+    }
 }
 
 object Shed: Building {
@@ -26,6 +34,9 @@ object Shed: Building {
     override val text = "1 (Point). May be constructed on any empty square in your town"
     override val shape = Shape(listOf(listOf(WOOD, STONE)))
     override val canBeBuiltAnywhere = true
+    override fun score(board: Map<Location, GamePiece>): Int {
+        return 0
+    }
 }
 
 object Well: Building {
@@ -34,4 +45,7 @@ object Well: Building {
     override val text = "1 (Point) for each adjacent (Cottage)"
     override val shape = Shape(listOf(listOf(WOOD, STONE)))
     override val canBeBuiltAnywhere = false
+    override fun score(board: Map<Location, GamePiece>): Int {
+        return 0
+    }
 }
